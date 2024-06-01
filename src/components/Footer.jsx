065d6ac7e50be28917091d72ed985c5618ad9130
sapirtalk <sapirtalk@gmail.com>
2024-06-02 00:33:59 +0300

    added link to facebook page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import { footer } from "@/styles/tailwindSaved"
 import { FaFacebook } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
 import { SiWhatsapp } from "react-icons/si";
+import Link from "next/link";
 
 
 const Footer = () => {
@@ -10,9 +11,15 @@ const Footer = () => {
             <div className={footer.container}>
                 <div className={footer.col}>
                     <div className={footer.row}>
-                        <FaFacebook size={45} color={'#1877F2'} />
-                        <BsInstagram size={45} color={'#E1306C'} />
-                        <SiWhatsapp size={45} color={'#25d366'} />
+                        <Link href='https://www.facebook.com/share/1oESuWaY789A8iUT/'>
+                            <FaFacebook size={45} color={'#1877F2'} />
+                        </Link>
+                        <Link href='https://www.facebook.com/share/1oESuWaY789A8iUT/'>
+                            <BsInstagram size={45} color={'#E1306C'} />
+                        </Link>
+                        <Link href='https://www.facebook.com/share/1oESuWaY789A8iUT/'>
+                            <SiWhatsapp size={45} color={'#25d366'} />
+                        </Link>
                     </div>
                 </div>
                 <div>
