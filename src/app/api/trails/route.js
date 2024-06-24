b196e360df1cd8810979f38f00a7e6db71f2e96e
GaLTa4 <galtamar4@gmail.com>
2024-06-24 16:02:24 +0300

    added admin_panel section

--- src/app/api/trails/route.js
+++ src/app/api/trails/route.js
@@ -1,36 +1,36 @@
-// import { connectToDatabase } from '../middleware/mongo';
-// import { NextResponse } from 'next/server';
+import { connectToDatabase } from '../middleware/mongo';
+import { NextResponse } from 'next/server';
 
 
 
 
-// export async function GET() {
-//     const db = await connectToDatabase();
-//     let id = null;
-//     let count = null;
+export async function GET() {
+    const db = await connectToDatabase();
+    let id = null;
+    let count = null;
 
 
-//     if (params){
-//         const url = new URL(request.url);
-//         count = url.searchParams.get('count');
-//         id = url.searchParams.get('id');
-//     }
+    if (params){
+        const url = new URL(request.url);
+        count = url.searchParams.get('count');
+        id = url.searchParams.get('id');
+    }
     
-//     // if id is provided, return the trail with that id
-//     if (id){
-//         console.log('id from GET trail req:', id);
-//         const trail = await db.collection('Trails').findOne({id});
-//         return NextResponse.json({ trail });
-//     }
-//     // if count is provided, return the first count trails
-//     if (count){
-//         console.log('count from GET trail req:', count);
-//         const trails = await db.collection('Trails').find({}).limit(parseInt(count)).toArray();
-//         return NextResponse.json({ trails });
-//     }
+    // if id is provided, return the trail with that id
+    if (id){
+        console.log('id from GET trail req:', id);
+        const trail = await db.collection('Trails').findOne({id});
+        return NextResponse.json({ trail });
+    }
+    // if count is provided, return the first count trails
+    if (count){
+        console.log('count from GET trail req:', count);
+        const trails = await db.collection('Trails').find({}).limit(parseInt(count)).toArray();
+        return NextResponse.json({ trails });
+    }
 
-//     // if no id or count is provided, return all trails
-//     console.log('no id or count from GET trail req , returning all trails');
-//     const trails = await db.collection('Trails').find({}).toArray();
-//     return NextResponse.json({ trails });
-// }
+    // if no id or count is provided, return all trails
+    console.log('no id or count from GET trail req , returning all trails');
+    const trails = await db.collection('Trails').find({}).toArray();
+    return NextResponse.json({ trails });
+}
