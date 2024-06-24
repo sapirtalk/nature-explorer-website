796ddbfa29ac4f25b97eab5ae9602c120c35d69a
GaLTa4 <galtamar4@gmail.com>
2024-06-24 16:10:49 +0300

    fixed

--- src/app/api/trails/route.js
+++ src/app/api/trails/route.js
@@ -2,40 +2,6 @@ import { connectToDatabase } from '../middleware/mongo';
 import { NextResponse } from 'next/server';
 
 
-<<<<<<< HEAD
-
-
-export async function GET() {
-    const db = await connectToDatabase();
-    let id = null;
-    let count = null;
-
-
-    if (params){
-        const url = new URL(request.url);
-        count = url.searchParams.get('count');
-        id = url.searchParams.get('id');
-    }
-    
-    // if id is provided, return the trail with that id
-    if (id){
-        console.log('id from GET trail req:', id);
-        const trail = await db.collection('Trails').findOne({id});
-        return NextResponse.json({ trail });
-    }
-    // if count is provided, return the first count trails
-    if (count){
-        console.log('count from GET trail req:', count);
-        const trails = await db.collection('Trails').find({}).limit(parseInt(count)).toArray();
-        return NextResponse.json({ trails });
-    }
-
-    // if no id or count is provided, return all trails
-    console.log('no id or count from GET trail req , returning all trails');
-    const trails = await db.collection('Trails').find({}).toArray();
-    return NextResponse.json({ trails });
-}
-=======
 // request body example:
 // {
 //     filterReq: {
@@ -96,4 +62,3 @@ const MakeQuery = (filterReq) => {
     return filterArr.length > 0 ? { $and: filterArr } : {};
 
 }
->>>>>>> 2d4034136f000d9ac6a34a972e90dde2f510735d
