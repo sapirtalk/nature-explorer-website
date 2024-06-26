e3cac9d8661e2152acf0ecdef7525e602f43d02e
meidan23 <meidan23@campus.technion.ac.il>
2024-06-26 16:23:57 +0300

    updated contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "@nextui-org/link";
 
 function Contact() {
   return (
@@ -8,16 +9,16 @@ function Contact() {
       <br/>
       <p>נשמח לשמוע מכם! ניתך ליצור איתנו קשר באמצעות כתובת המייל הבאה:</p>
       <br/>
-      <a href='mailto:ntr.explorer1@gmail.com' className='underline underline-offset-auto'>ntr.explorer1@gmail.com</a>
+      <Link isBlock showAnchorIcon href="mailto:ntr.explorer1@gmail.com" color="secondary" className='underline underline-offset-auto font-bold'>ntr.explorer1@gmail.com</Link>
       <br/>
       <br/>
       <p>מוזמנים להצטרף גם אל קבוצת הפייסבוק שלנו </p>
-      <a href='https://www.facebook.com/groups/2787205004828441/' className='underline underline-offset-auto'>״בשבילי חיפה - שבילים , מסלולים, פינות טבע בעיר״</a>
+      <Link isBlock showAnchorIcon href="https://www.facebook.com/groups/2787205004828441/" color="secondary" className='underline underline-offset-auto'>״בשבילי חיפה - שבילים , מסלולים, פינות טבע בעיר״</Link>
       <br/>
       <br/>
       <p>שימו לב, האתר מנוהל על ידי מתנדבים, ולכן ייתכן שמענה לפנייה ייקח קצת זמן</p>
       <br/>
-      <p><span dir='rtl'>תודה על הסבלנות, נחזור אליכם בהקדם!</span></p>
+      <p>תודה על הסבלנות, נחזור אליכם בהקדם!</p>
       </span>
     </div>
   )
