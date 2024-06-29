0d1ba38d9711ad854c733fc98fa1c19c8b102f15
GaLTa4 <galtamar4@gmail.com>
2024-06-29 23:31:13 +0300

    modification for less files

--- src/app/api/user_panel/favorite_trails/route.js
+++ src/app/api/user_panel/favorite_trails/route.js
@@ -1,6 +1,6 @@
 import { connectToDatabase } from '../../middleware/mongo';
 import { NextResponse } from 'next/server';
-
+import { ObjectId } from 'mongodb'; // Import ObjectId from mongodb
 
 // POST /api/user_panel/favorite_trails
 // Purpose:
@@ -13,7 +13,6 @@ export async function POST(req) {
     try {
         const { requesterId } = await req.json();
         const db = await connectToDatabase();
-        var ObjectId = require('mongodb').ObjectId;
 
         const requester = await db.collection('Users').findOne({_id: new ObjectId(requesterId)})
 
@@ -31,5 +30,48 @@ export async function POST(req) {
         return NextResponse.json({ success: true, favorite_trails: requesterFavoriteTrails });
     } catch (error) {
         return NextResponse.json({ success: false, message: error.message });
+    }
+}
+
+
+// PUT /api/user_panel/favorite_trails
+// Purpose:
+// Allow a user to add/delete a specific trail to/from his favorite trails
+// Input Example:
+// {
+//   "userId": "667dcb312f4666fa50754115",
+//   "trailId": "667dc4272f4666fa50754112",
+//   "action": "add"
+// }
+export async function PUT(req) {
+    try {
+        const { userId, trailId, action } = await req.json();
+        const db = await connectToDatabase();
+
+        const response = await updateUserById(db, userId, trailId, action);
+        return NextResponse.json(response);
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
+
+async function updateUserById(db, userId, trailId, action) {
+    const updateOperation = action === "add" 
+        ? { $addToSet: { favoriteTrails: trailId } }  // Add trail to set, prevents duplicates
+        : { $pull: { favoriteTrails: trailId } };  // Remove trail from set if exists
+
+    try {
+        const result = await db.collection('Users').updateOne(
+            { _id: new ObjectId(userId) },
+            updateOperation
+        );
+
+        if (result.matchedCount > 0) {
+            return { success: true, message: 'Favorite trails updated successfully.' };
+        } else {
+            return { success: false, message: 'User not found.' };
+        }
+    } catch (error) {
+        return { success: false, message: error.message };
     }
 }
