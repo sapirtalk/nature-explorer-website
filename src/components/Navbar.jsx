9525e462c73a638257a5538049496830f6531895
sapirtalk <sapirtalk@gmail.com>
2024-07-01 14:50:25 +0300

    nav icon sizes + backend merge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,13 +112,13 @@ const Navbar = () => {
           <Tooltip placement='bottom' content={<div className='text-xl p-1'>הרשמה</div>} shadow='none' showArrow>  
             <Link href='/register' className={curPath == '/register' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/register')}>
               <p className={navbar.desktop_p}>הרשמה</p>
-              <GiArchiveRegister size={40}/>
+              <GiArchiveRegister className='xl:text-[40px] lg:text-[30px]'/>
             </Link>
           </Tooltip>
           <Tooltip placement='bottom' content={<div className='text-xl p-1'>התחברות</div>} shadow='none' showArrow>
             <Link href='/login' className={curPath == '/login' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/login')}>
               <p className={navbar.desktop_p}>התחברות</p>
-              <BiLogIn size={40} />
+              <BiLogIn className='xl:text-[40px] lg:text-[30px]' />
             </Link>
           </Tooltip>
         </div>
@@ -127,13 +127,13 @@ const Navbar = () => {
           <Tooltip placement='bottom' content={<div className='text-xl p-1'>הפרופיל שלי</div>} shadow='none' showArrow>
           <Link href='/profile' className={curPath == '/profile' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/profile')}>
             <p className={navbar.desktop_p}>{user.firstName}</p>
-            <CgProfile size={40} />
+            <CgProfile className='xl:text-[40px] lg:text-[30px]' />
           </Link>
         </Tooltip>
         <Tooltip placement='bottom' content={<div className='text-xl p-1'>התנתק</div>} shadow='none' showArrow>
           <button onClick={handleLogout} className='mx-5 flex items-center flex-row hover:text-blue-500'>
             <p className={navbar.desktop_p}>התנתק</p>
-            <BiLogOut size={40} />
+            <BiLogOut className='xl:text-[40px] lg:text-[30px]' />
           </button>  
         </Tooltip>
         </div>   
@@ -145,37 +145,37 @@ const Navbar = () => {
             <Tooltip placement='bottom' content={<div className='text-xl p-1'>דף הבית</div>} shadow='none' showArrow>
             <Link onClick={() => setPath('/home')} className={curPath == '/home' ? navbar.desktop_link_curr : navbar.desktop_link} href='/home'>
               <p className={navbar.desktop_p}>דף הבית</p>
-              <IoHome size={40} />
+              <IoHome className='xl:text-[40px] lg:text-[30px]' />
             </Link>
             </Tooltip>
             <Tooltip placement='bottom' content={<div className='text-xl p-1'>סיורים</div>} shadow='none' showArrow>
             <Link className={curPath == '/tours' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/tours')} href='/tours'>
               <p className={navbar.desktop_p}>סיורים</p>
-              <FaFlag size={40} />
+              <FaFlag className='xl:text-[40px] lg:text-[30px]' />
             </Link>
             </Tooltip>
             <Tooltip placement='bottom' content={<div className='text-xl p-1'>אודות</div>} shadow='none' showArrow>
             <Link href='/about' className={curPath == '/about' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/about')}>
               <p className={navbar.desktop_p}>אודות</p>
-              <BsFillInfoCircleFill size={40} />
+              <BsFillInfoCircleFill className='xl:text-[40px] lg:text-[30px]' />
             </Link>
             </Tooltip>
             <Tooltip placement='bottom' content={<div className='text-xl p-1'>צור קשר</div>} shadow='none' showArrow>
             <Link href='/contact' className={curPath == '/contact' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/contact')}>
               <p className={navbar.desktop_p}>צור קשר</p>
-              <AiFillMessage size={40} />
+              <AiFillMessage className='xl:text-[40px] lg:text-[30px]' />
             </Link>
             </Tooltip>
             <Tooltip placement='bottom' content={<div className='text-xl p-1'>כתבות</div>} shadow='none' showArrow>
             <Link href = '/articles' className={curPath == '/articles' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/articles')}>
               <p className={navbar.desktop_p}>כתבות</p>
-              <IoNewspaperSharp size={40} />
+              <IoNewspaperSharp className='xl:text-[40px] lg:text-[30px]' />
             </Link>
             </Tooltip>
             <Tooltip  placement='bottom' content={<div className='text-xl p-1'>מסלולים</div>} shadow='none' showArrow>
             <Link href = '/trail_catalogue' className={curPath == '/trail_catalogue' ? navbar.desktop_link_curr : navbar.desktop_link} onClick={() => setPath('/trail_catalogue')}>
               <p className={navbar.desktop_p}>מסלולים</p>
-              <FaMapSigns size={40} />
+              <FaMapSigns className='xl:text-[40px] lg:text-[30px]' />
             </Link>
             </Tooltip>
           </ul>
