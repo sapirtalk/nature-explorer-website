3ef2c5ebe657322daca2196b2ab68c1e84c90d4b
GaLTa4 <galtamar4@gmail.com>
2024-07-18 22:45:55 +0300

    fixed new user's ability to register to tours

--- src/app/api/users/register/route.js
+++ src/app/api/users/register/route.js
@@ -21,7 +21,7 @@ export async function POST(req) {
         fromFacebook: fromFacebook,
         LastLogin: new Date(),
         RegisterDate: new Date(),
-        registeredTours: [],
+        registeredTours: {},
         favoriteTrails: []
     };
 
